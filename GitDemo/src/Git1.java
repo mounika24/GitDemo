47e434c25f470ab0cc035cd0f82fa6f983afafb0
gbalipalli <gbalipalli@Guest792.corp.miraclesoft.com>
2018-01-10 20:13:51 +0530

    added java file

---
+++ GitDemo/src/Git1.java
@@ -0,0 +1,7 @@
+
+public class Git1 {
+	public static void main(){
+		System.out.println("Welcome");
+	}
+
+}
--- Sample1/src/sample/Demo.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-public class Demo {
-	public static void main(String args[]){
-		System.out.println("Welcome git");
-	}
-
-}
